b2828800c8567dc3cc0a8f79cb896cea532b4e3c
sai charan <saicharannwm@gmail.com>
2022-11-08 19:43:00 -0600

    modified resource.js file where amazon is replaced by amazons

--- routes/resource.js
+++ routes/resource.js
@@ -13,18 +13,18 @@ router.get('/', api_controller.api);
 /// COSTUME ROUTES /// 
  
 // POST request for creating a Costume.  
-router.post('/Amazon', Amazon_controller.Amazon_create_post); 
+router.post('/Amazons', Amazon_controller.Amazon_create_post); 
  
 // DELETE request to delete Costume. 
-router.delete('/Amazon/:id', Amazon_controller.Amazon_delete); 
+router.delete('/Amazons/:id', Amazon_controller.Amazon_delete); 
  
 // PUT request to update Costume. 
-router.put('/Amazon/:id', Amazon_controller.Amazon_update_put); 
+router.put('/Amazons/:id', Amazon_controller.Amazon_update_put); 
  
 // GET request for one Costume. 
-router.get('/Amazon/:id', Amazon_controller.Amazon_detail); 
+router.get('/Amazons/:id', Amazon_controller.Amazon_detail); 
  
 // GET request for list of all Costume items. 
-router.get('/Amazon', Amazon_controller.Amazon_list); 
+router.get('/Amazons', Amazon_controller.Amazon_list); 
  
 module.exports = router; 
