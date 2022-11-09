c74e1bbdd5b859ff490926ac888ed48f097862e3
sai charan <saicharannwm@gmail.com>
2022-11-08 19:58:52 -0600

    added stubs

--- controllers/Amazon.js
+++ controllers/Amazon.js
@@ -4,10 +4,34 @@ var Amazon = require('../models/Amazon');
 exports.Amazon_list = function(req, res) { 
     res.send('NOT IMPLEMENTED: Amazon list'); 
 }; 
+
+// List of all Costumes 
+exports.Amazon_list = async function(req, res) { 
+    try{ 
+        theAmazons = await Amazon.find(); 
+        res.send(theAmazons); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
+}; 
  
 // for a specific Costume. 
 exports.Amazon_detail = function(req, res) { 
     res.send('NOT IMPLEMENTED: Amoazon detail: ' + req.params.id); 
+}; 
+
+// List of all Costumes 
+exports.Amazon_detail = async function(req, res) { 
+    try{ 
+        theAmazons = await Amazon.find(); 
+        res.send(theAmazons); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
 }; 
  
 // Handle Costume create on POST. 
@@ -15,12 +39,48 @@ exports.Amazon_create_post = function(req, res) {
     res.send('NOT IMPLEMENTED: Amazon create POST'); 
 }; 
  
+// List of all Costumes 
+exports.Amazon_create_post = async function(req, res) { 
+    try{ 
+        theAmazons = await Amazon.find(); 
+        res.send(theAmazons); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
+}; 
+
 // Handle Costume delete form on DELETE. 
 exports.Amazon_delete = function(req, res) { 
     res.send('NOT IMPLEMENTED: Amazon delete DELETE ' + req.params.id); 
+}; 
+
+// List of all Costumes 
+exports.Amazon_delete = async function(req, res) { 
+    try{ 
+        theAmazons = await Amazon.find(); 
+        res.send(theAmazons); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
 }; 
  
 // Handle Costume update form on PUT. 
 exports.Amazon_update_put = function(req, res) { 
     res.send('NOT IMPLEMENTED: Amazon update PUT' + req.params.id); 
+}; 
+
+// List of all Costumes 
+exports.Amazon_update_put = async function(req, res) { 
+    try{ 
+        theAmazons = await Amazon.find(); 
+        res.send(theAmazons); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
 }; 
