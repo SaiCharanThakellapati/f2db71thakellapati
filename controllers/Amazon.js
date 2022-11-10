6fc596013fbda3162346881de40c006d840819a1
sai charan <saicharannwm@gmail.com>
2022-11-09 23:00:34 -0600

    added files

--- controllers/Amazon.js
+++ controllers/Amazon.js
@@ -97,3 +97,24 @@ exports.Amazon_view_all_Page = async function(req, res) {
         res.send(`{"error": ${err}}`); 
     }   
 }; 
+
+// Handle Costume create on POST. 
+exports.Amazon_create_post = async function(req, res) { 
+    console.log(req.body) 
+    let document = new Amazon(); 
+    // We are looking for a body, since POST does not have query parameters. 
+    // Even though bodies can be in many different formats, we will be picky 
+    // and require that it be a json object 
+    // {"costume_type":"goat", "cost":12, "size":"large"} 
+    document.item = req.body.item; 
+    document.quantity = req.body.quantity; 
+    document.cost = req.body.cost; 
+    try{ 
+        let result = await document.save(); 
+        res.send(result); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
+}; 
